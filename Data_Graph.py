393a3a5452c569c0d10e1ab5b76e97e5f78ad51c
Aaris Kazi <40735215+Aaris-Kazi@users.noreply.github.com>
2020-03-20 01:21:28 +0530

    Add files via upload

--- Data_Graph.py
+++ Data_Graph.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 x = [1,2,3]
 y = [5,4,9]
 z = [4,9,6]
 plt.plot(x,y)
 plt.plot(x,z)
+
 plt.title('Data Graph')
 plt.xlabel('X')
 plt.ylabel('Y AND Z')
@@ -20,11 +22,18 @@ i = test_data[test_data.Name == 'Afif plaavkar']
 print(i)
 j = test_data[test_data.Name == 'Sakib Arkate']
 print(j)
+k = test_data[test_data.Name == 'Khan Osama']
+print(j)
 
 plt.plot(i.Subject,i.Attendance)
 plt.plot(j.Subject,j.Attendance)
-plt.legend(['Afif', 'Sakib'])
+plt.plot(k.Subject,k.Attendance)
+plt.legend(['Afif', 'Sakib','Osama'])
 plt.xlabel('Subject Code')
 plt.ylabel('Attendace Percentage')
 plt.grid(True)
+plt.title('Attendance of every subject')
 plt.show()
+
+result = test_data[test_data.Subject =='AVG TH + PR']
+print(result)
